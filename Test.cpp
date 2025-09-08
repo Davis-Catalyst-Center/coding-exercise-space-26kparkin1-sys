18f48761aaacfdfb7b94ce30f72e690bb6b0582b
26kparkin1-sys <26kparkin1@go.dsdmail.net>
2025-09-08 15:11:48 +0000

    fix

--- Test.cpp
+++ Test.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
-#include <string>
 
-int main()
-{
-    float num = 3.14;
-    int x = num;
+void printCats(int numCats);
+int doubleNum(int num, int num2);
 
-    return 0;
+int main() {
+printCats (7);
+int result = doubleNum(5, 2);
+std::cout << result << std::endl;
+
+}
+int doubleNum(int num, int num2){
+    return num * num2;
+
+}
+
+void printCats(int numCats) {
+    if(numCats > 20){
+        return;
+
+    }
+for(int i = 0; i < numCats; i++){
+std::cout << "Cats" << std::endl;
+}
+
 }
--- ce6.cpp
+++ ce6.cpp
@@ -1,1 +1,65 @@
+#include <iostream>
+#include <cmath>
+ 
+bool isEven(int number);
+int sumOfDigits(int number);
+bool isPrime(int number);
+bool isSpecialNumber(int number);
 
+bool isEven(int number) {
+    return (number % 2 == 0);
+}
+
+int sumOfDigits(int number) {
+    int sum = 0;
+    number = std::abs(number); 
+    while (number > 0) {
+        sum += number % 10; 
+        number /= 10;      
+    }
+    return sum;
+}
+
+bool isPrime(int number) {
+    if (number <= 1) {
+        return false;
+    }
+    for (int i = 2; i <= std::sqrt(number); ++i) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isSpecialNumber(int number) {
+    return isEven(number) && isPrime(sumOfDigits(number));
+}
+
+int main() {
+    int inputNumber;
+    std::cout << "Enter a number: " << std::endl;
+    std::cin >> inputNumber;
+
+    if (isEven(inputNumber)) {
+        std::cout << inputNumber << " is an even number." << std::endl;
+    } else {
+        std::cout << inputNumber << " is an odd number." << std::endl;
+    }
+
+    std::cout << "The sum of your digits is: " << sumOfDigits(inputNumber) << std::endl;
+
+    if (isPrime(inputNumber)) {
+        std::cout << "The number " << inputNumber << " is prime." << std::endl;
+    } else {
+        std::cout << "The number " << inputNumber << " is not prime." << std::endl;
+    }
+    
+    if (isSpecialNumber(inputNumber)) {
+        std::cout << "The number " << inputNumber << " is special." << std::endl;
+    } else {
+        std::cout << "The number " << inputNumber << " is not special." << std::endl;
+    }
+
+    return 0;
+}
