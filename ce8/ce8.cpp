5c7fed3a97b5e75107ae3ae95ab47a95e6980492
26kparkin1-sys <26kparkin1@go.dsdmail.net>
2025-09-12 19:11:45 +0000

    wizard

--- ce8/ce8.cpp
+++ ce8/ce8.cpp
@@ -5,9 +5,13 @@ using namespace std;
 int main() {
     Wizard w1("Merlin", "Fire", 10, 100);
     Wizard w2("Morgana", "Ice", 12, 120);
+    Wizard w3("Dumbledore", "Air", 14, 150);
+    Wizard w4("Cassandra", "Rock", 11, 135);
 
     cout << w1.displayInfo() << endl;
     cout << w2.displayInfo() << endl;
+    cout << w3.displayInfo() << endl;
+    cout << w4.displayInfo() << endl;
 
     w1.castSpell(w2, 30);
     cout << w2.displayInfo() << endl;
